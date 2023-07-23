c23643fa81d7ed39e51d5467976bd51e0baf2abd
Gustavo Aranciba <gusaranciba@gmail.com>
2023-07-23 00:16:40 -0300

    My first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <h1>Control de Gastos</h1>
+    </>
+  )
+}
+
+export default App
