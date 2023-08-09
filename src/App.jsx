816774df86193ed65211cdd6ebefeffe174c9244
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-09 19:35:25 -0300

    Presupuesto no válido, Mensaje.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import viteLogo from '/vite.svg'
 import Header from './components/Header';
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState(0); 
 
   return (
-    <>
+    
       <div>
-        <Header></Header>
+        <Header
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+        />
       </div>
-    </>
+    
   )
 }
 
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import {useState} from 'react';
+import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+    if (!Number(presupuesto) || Number(presupuesto) < 0) {
+      setMensaje('No es un presupuesto válido')
+    } else {
+      console.log('Si es un presupuesto válido')
+    }
+  }
+  
   return (
     <div className='contenedor-presupuesto contenedor sombra' >
-        <form action="" className='formulario' >
+        <form onSubmit={handlePresupuesto} className='formulario' >
             <div className='campo' >
-                <label htmlFor="">Definir Presupuesto</label>
+                <label >Definir Presupuesto</label>
                 <input
                     type="text"
                     className='nuevo-presupuesto'
                     placeholder='Añade tu presupuesto'
+                    value={presupuesto}
+                    onChange={e => setPresupuesto(e.target.value)}
                 />
             </div>
             <input type="submit" value="Añadir" />
+            {mensaje && <Mensaje tipo="error" >{mensaje}</Mensaje> }
         </form>
     </div>
   )
