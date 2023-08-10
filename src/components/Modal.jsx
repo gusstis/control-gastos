a7aa6b50b05353077a9e4e2a71abfecf2fb4d782
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-10 10:36:36 -0300

    Formulario Nuevo Gasto

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,6 +19,36 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
         </div>
         <form className={` formulario ${ animarModal ? "animar" : "cerrar" } `} >
             <legend>Nuevo Gasto</legend>
+            <div className='campo' >
+                <label htmlFor="nombre">Nombre Gasto</label>
+                <input
+                    id='nombre'
+                    type="text"
+                    placeholder='Comida, salidas, remedios, etc.'
+                />
+            </div>
+            <div className='campo' >
+                <label htmlFor="cantidad">Cantidad</label>
+                <input
+                    id='cantidad'
+                    type="number"
+                    placeholder='Monto del gasto: ej.: 250'
+                />
+            </div>
+            <div className='campo' >
+                <label htmlFor="categoria">Categoría</label>
+                <select name="" id="categoria">
+                    <option value="">······Seleccione······</option>
+                    <option value="ahoro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="gastos">Gastos Varios</option>
+                    <option value="salud">Salud</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="suscripciones">Suscripciones</option>
+                </select>
+            </div>
+            <input type="submit" value="Añadir Gasto" />
         </form>
     </div>
   )
