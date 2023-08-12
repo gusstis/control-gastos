6803a5e7587c5504591a7230db9d5ae9bdfe51bc
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-12 20:36:57 -0300

    Adding swipe effect with react-swipeable-list

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,3 +1,11 @@
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions
+} from 'react-swipeable-list'
+import "react-swipeable-list/dist/styles.css"
 import {formatearFecha} from '../helpers'
 import IconoAhorro from '../img/icono_ahorro.svg'
 import IconoCasa from '../img/icono_casa.svg'
@@ -20,8 +28,21 @@ const diccionarioIconos = {
 
 const Gasto = ({gasto}) => {
     const {categoria, nombre, cantidad, id, fecha} = gasto
+
+const leadingActions = () => {
+  console.log('Editar···>')
+}
+const trailingActions = () => {
+  console.log('<─·──Eliminar')
+}
+
   return (
-    <div className='gasto sombra' >
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions}
+        trailingActions={trailingActions}
+      >
+      <div className='gasto sombra' >
         <div className='contenido-gasto' >
           <img src={diccionarioIconos[categoria]} alt="Icono Gasto" />
             <div className='descripcion-gasto' >
@@ -37,6 +58,8 @@ const Gasto = ({gasto}) => {
               $ {cantidad}
             </p>
     </div>
+      </SwipeableListItem>
+    </SwipeableList>
   )
 }
 
