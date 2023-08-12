0223e38c9f5f54176a75834a90377c315bffeab6
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-12 19:48:23 -0300

    Calculando lo gastado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import ControlPresupuesto from './ControlPresupuesto';
 
 const Header = ({
   presupuesto,
+  gastos,
   setPresupuesto,
   isValidPresupuesto,
   setIsValidPresupuesto
@@ -13,6 +14,7 @@ const Header = ({
         <h1>Planificador de Gastos</h1>
         {isValidPresupuesto ? (
           <ControlPresupuesto
+            gastos={gastos}
             presupuesto={presupuesto}
           />
         ) : (
