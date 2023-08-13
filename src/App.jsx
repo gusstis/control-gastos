3ce964fc459770df37c67a556e429bb94d841c75
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-12 21:50:50 -0300

    Detectando el Gasto a editar al hacer swipe

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 //import viteLogo from '/vite.svg'
 import Header from './components/Header';
 import Modal from './components/Modal'
@@ -13,6 +13,14 @@ function App() {
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
   const [gastos, setGastos] = useState([])
+  const [gastoEditar, setGastoEditar] = useState({})
+  useEffect(() => {
+    if (Object.keys(gastoEditar).length > 0) {
+      handleNuevoGasto()
+    }
+    
+  }, [gastoEditar])
+  
 
 
   const handleNuevoGasto = () => {
@@ -47,6 +55,7 @@ function App() {
             <main>
             <ListadoGastos
               gastos={gastos}
+              setGastoEditar={setGastoEditar}
             />
             </main>
             <div className='nuevo-gasto' >
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -26,12 +26,12 @@ const diccionarioIconos = {
                     suscripciones: IconoSuscripciones
 }
 
-const Gasto = ({gasto}) => {
+const Gasto = ({gasto, setGastoEditar}) => {
     const {categoria, nombre, cantidad, id, fecha} = gasto
 
 const leadingActions = () => (
   <LeadingActions>
-    <SwipeAction onClick={() => console.log('editing')} >
+    <SwipeAction onClick={() => setGastoEditar(gasto)} >
       Editar
     </SwipeAction>
   </LeadingActions>
