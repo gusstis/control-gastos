aea0e216241027366521cf814129caf0f1703fc3
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-19 11:17:58 -0300

    Guardando los gastos en Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import {generarId} from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
-  const [gastos, setGastos] = useState([])
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  )
   const [presupuesto, setPresupuesto] = useState(
     Number(localStorage.getItem('presupuesto')) ?? 0
   );
@@ -28,6 +30,12 @@ function App() {
   useEffect(() => {
     localStorage.setItem('presupuesto', presupuesto ?? 0)    
   }, [presupuesto])
+
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])  
+  
+  }, [gastos])
+  
   
   useEffect(() => {
     const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0
