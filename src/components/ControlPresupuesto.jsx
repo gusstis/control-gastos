e4cf33f8108014f57660069533871900ee4f694f
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-19 09:50:33 -0300

    La gráfica circular se define por el porcentaje acumulado de gastos

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -4,14 +4,20 @@ import 'react-circular-progressbar/dist/styles.css';
 
 const ControlPresupuesto = ({gastos, presupuesto}) => {
 
+    const [porcentaje, setPorcentaje] = useState(0)
     const [disponible, setDisponible] = useState(0)
     const [gastado, setGastado] = useState(0)
     useEffect(() => {
         
         const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0 )
         const totalDisponible = presupuesto - totalGastado 
+        //Cálculo de porcentaje gastado
+        const nuevoPorcentaje = (((totalGastado) / presupuesto) * 100).toFixed(2)
         setDisponible(totalDisponible)
         setGastado(totalGastado)
+        setTimeout(() => {
+            setPorcentaje(nuevoPorcentaje)
+        }, 999);
       
     }, [gastos])
     
@@ -28,7 +34,7 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
     <div className='contenedor-presupuesto contenedor sombra dos-columnas' >
         <div>
             <CircularProgressbar
-                value={10}
+                value={porcentaje}
             />
         </div>
         <div className='contenido-presupuesto' >
